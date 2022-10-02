79d1f65003acf3a1a9baa90eb80fcaba2e294d96
Bob Jackson <1187688895@qq.com>
2022-10-02 22:12:29 +0800

    use onClick to setState

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,17 +1,21 @@
 import {FaStar} from "react-icons/fa";
 import {useState} from "react";
 
-const Star = ({selected = false}) => (
-    <FaStar color={selected ? "red" : "grey"}/>
+const Star = ({selected = false, onSelect = f => f}) => (
+    <FaStar color={selected ? "red" : "grey"} onClick={onSelect}/>
 );
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({length: totalStars = 5}) {
-    const [selectedStars] = useState(3);
+export default function StarRating({totalStars = 5}) {
+    const [selectedStars, setSelectedStars] = useState(0);
     return (
         <>
-            {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i}/>)}
+            {createArray(totalStars).map((n, i) =>
+                <Star key={i}
+                      selected={selectedStars > i}
+                      onSelect={() => setSelectedStars(i + 1)}
+                />)}
             <p>
                 {selectedStars} of {totalStars} stars
             </p>
