eb4e260988cb49598b299605d427ef90bba4d4e9
Bob Jackson <1187688895@qq.com>
2022-10-02 20:55:19 +0800

    useState

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import {FaStar} from "react-icons/fa";
+import {useState} from "react";
 
 const Star = ({selected = false}) => (
     <FaStar color={selected ? "red" : "grey"}/>
@@ -6,6 +7,14 @@ const Star = ({selected = false}) => (
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({length = 5}) {
-    return createArray(length).map((n, i) => <Star key={i}/>);
+export default function StarRating({length: totalStars = 5}) {
+    const [selectedStars] = useState(3);
+    return (
+        <>
+            {createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i}/>)}
+            <p>
+                {selectedStars} of {totalStars} stars
+            </p>¬
+        </>
+    );
 }
