501b737909cab5d9035709071560d1934d78fe9e
Bob Jackson <1187688895@qq.com>
2022-10-02 20:47:10 +0800

    easy FaStar array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {FaStar} from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      [
+          <FaStar color="red"/>,
+          <FaStar color="red"/>,
+          <FaStar color="red"/>,
+          <FaStar color="grey"/>,
+          <FaStar color="grey"/>,
+      ]
   );
 }
 
---
+++ src/StarRating.js
@@ -0,0 +1,11 @@
+import {FaStar} from "react-icons/fa";
+
+const Star = ({selected = false}) => (
+    <FaStar color={selected ? "red" : "grey"}/>
+);
+
+const createArray = length => [...Array(length)];
+
+export default function StarRating({length = 5}) {
+    return createArray(length).map((n, i) => <Star key={i}/>);
+}
